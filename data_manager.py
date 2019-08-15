b6d9f0d931898e98c00d24a9def68f7dd88f71a6
Drosztmér István <drosztmer.istvan@gmail.com>
2019-08-15 10:48:29 +0200

    Add search

--- data_manager.py
+++ data_manager.py
@@ -138,3 +138,22 @@ def delete_an_answer(cursor, id_to_delete):
                     DELETE FROM answer WHERE id = %(id_to_delete)s
                     """,
                    {'id_to_delete': id_to_delete})
+
+
+@database_common.connection_handler
+def search(cursor, phrase):
+    cursor.execute("""
+                    SELECT DISTINCT id, title, submission_time
+                    FROM question
+                    WHERE
+                        title LIKE CONCAT('%', %(phrase)s, '%')
+                    OR message LIKE CONCAT('%', %(phrase)s, '%')
+                    OR id IN
+                        (SELECT question_id
+                        FROM answer
+                        WHERE
+                            message LIKE CONCAT('%', %(phrase)s, '%'));
+                    """,
+                   {'phrase': phrase})
+    search_results = cursor.fetchall()
+    return search_results
