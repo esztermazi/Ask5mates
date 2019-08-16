427b15fed779d8b03be131e4fc5f6ff48039944e
Eszter Kobor <kobor.eszter@gmail.com>
2019-08-16 10:42:30 +0200

    delete unused functions about time

--- util.py
+++ util.py
@@ -1,13 +1,4 @@
 from datetime import datetime
-import time
-
-
-def convert_time(unix_time):
-    return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
-
-
-def get_unix_timestamp():
-    return int(time.time())
 
 
 def convert_linebreaks_to_br(original_str):
